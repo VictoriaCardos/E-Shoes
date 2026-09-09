8cefcac8e2b428323e8adea01a19240b63041fb0
Victoria Cardoso <vvjcerque@hotmail.com>
2026-09-09 16:00:20 -0300

    feat: create header component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,7 +2,23 @@ import styles from "./Header.module.css";
 const Header = () => {
   return (
     <header className={styles.header}>
-      <h1>E-Shoes</h1>
+      <div className={styles.topRow}>
+        <p>E-Shoes</p>
+        <input
+          type="search"
+          placeholder="Buscar tênis..."
+          className={styles.searchInput}
+        />
+        <button className={styles.accountButton}>Minha Conta</button>
+        <button className={styles.cartButton}>Carrinho</button>
+      </div>
+      <nav className={styles.categoryRow}>
+        <button className={styles.filterButton}>Masculino</button>
+        <button className={styles.filterButton}>Feminino</button>
+        <button className={styles.filterButton}>Footwear</button>
+        <button className={styles.filterButton}>Casual</button>
+        <button className={styles.filterButton}>Todos</button>
+      </nav>
     </header>
   );
 };
